db09f8cb15061b1d6c55c3228464d898455e9401
Diego Botero <diego.botero@correounivalle.edu.co>
2019-02-05 23:11:02 -0500

    Inciso 2, Displaying a list.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from './hero';
+import { HEROES } from './test-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -9,11 +10,21 @@ import { Hero } from './hero';
 })
 export class HeroesComponent implements OnInit {
 	
+    heroes = HEROES;
+    
+    selectedHero: Hero;
+    
+    /*
 	hero: Hero = {
 		id: 1,
 		name: 'NinjaStorm'
 	};
-	
+	*/
+    
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
+    
   constructor() { }
 
   ngOnInit() {
